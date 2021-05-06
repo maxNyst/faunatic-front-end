f0e674f7f3a8524d7d7e0fb192d65b6da2cc0a2b
Agnes <agnes.wo@hotmail.com>
2021-05-06 11:40:29 +0200

    -

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:firebase_core/firebase_core.dart'; //new
+import 'package:firebase_auth/firebase_auth.dart'; //new
+
 import 'package:faunatic_front_end/login_page.dart';
 import 'package:flutter/material.dart';
 
